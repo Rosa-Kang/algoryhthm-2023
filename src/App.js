291afb1929e11c610c8119b5df8cf1979f84ae7e
Rosa Kang <therosessom@gmail.com>
2023-07-28 10:29:47 -0700

    add navBar and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
-import logo from '../../public/images/logo.png';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/logo.png';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
   return (
       <div>
           <nav className="navbar">
               <div className="navbar-container">
-                  <Link to='/' className='navbar-logo'><img src={logo} alt="" /></Link>
+                <Link to='/' className='navbar-logo'><img src={logo} alt="site-logo" height='160' /></Link>
+                <div className="menu-icon" onClick={handleClick}>
+                  <i className={click ? 'fa-solid fa-xmark' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <li className='nav-item'>
+                      <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                  </li>
+                  <li className='nav-item'>
+                      <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                  </li>
+                  <li className='nav-item'>
+                      <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>Sign up</Link>
+                  </li>
+                  <li className='nav-item'>
+                      <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Contact</Link>
+                  </li>
+                </ul>
               </div>
           </nav>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
